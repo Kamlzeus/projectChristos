6f1f3da2391a4296c00e5ef3c8bc3323595981ba
Kamlzeus <yusufova_k@auca.kg>
2024-11-21 12:30:58 +0600

    рр

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import { useEffect, useState } from "react";
 import { ProductCard } from "./components/ProductCard";
+import "./index.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
-  
-  useEffect(()=> {
-    fetch('https://fakestoreapi.com/products')
-            .then((res)=>res.json())
-            .then((json)=>setProducts(json))
-            .finally(() => setLoading(false));
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json))
+      .finally(() => setLoading(false));
   }, []);
-  
+
   return (
-    <div>
-      {loading && <div>Loading...</div>}
-      {products.map((product) => (
-        <ProductCard data={product} />
-      ))}
+    <div className="app">
+      {loading && <div>Загрузка...</div>}
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard key={product.id} data={product} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,13 +1,18 @@
+import React from "react";
 import "./index.css";
 
 export function ProductCard(props) {
   const { image, category, title, price } = props.data;
+
   return (
     <div className="product-card">
-      <img width="150" src={image} alt="" />
-      <div>{category}</div>
-      <h4>{title}</h4>
-      <div>{price}</div>
+      <img className="product-image" src={image} alt={title} />
+      <div className="product-category">{category}</div>
+      <h4 className="product-title">{title}</h4>
+      <div className="price-button-container">
+        <div className="product-price">${price.toFixed(2)}</div>
+        <button className="add-to-cart">Add to cart</button>
+      </div>
     </div>
   );
 }
